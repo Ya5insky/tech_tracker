86799c993a62451cbfb9177870e5f3c815a7dad9
Artem Yasinscky <artemyasinscky@gmail.com>
2022-12-29 22:56:34 +0500

    modal udpate

--- app.js
+++ app.js
@@ -4,26 +4,67 @@ const backdrop = document.querySelector('#backdrop')
 const progress = document.querySelector('#progress')
 
 
-content.addEventListener('click', opendModal)
+content.addEventListener('click', openCard)
 backdrop.addEventListener('click', closeModal)
+modal.addEventListener('change', toggleTech)
+
+
+const APP_TITLE = document.title
 
 const technologies = [
     {title: 'HTML', description: 'HTML Text', type: 'html', done: true},
     {title: 'CSS', description: 'CSS Text', type: 'css', done: true},
     {title: 'JavaScript', description: 'JavaScript Text', type: 'js', done: false},
-    {title: 'Git', description: 'Git Text', type: 'git', done: true},
-    {title: 'React', description: 'React Text', type: 'react', done: true},
+    {title: 'Git', description: 'Git Text', type: 'git', done: false},
+    {title: 'React', description: 'React Text', type: 'react', done: false},
 ]
 
-function opendModal(event) {
+function openCard(event) {
     const data = event.target.dataset
     const tech = technologies.find(t => t.type === data.type)
     if (!tech) return
+
+    openModal(toModal(tech), tech.title)
+}
+
+function toModal(tech) {
+    const checked = tech.done ? 'checked' : ''
+    return `
+    <h2>${tech.title}</h2>
+    <p>
+      ${tech.description}
+    </p>
+    <hr />
+    <div>
+      <input type="checkbox" id="done" ${checked} data-type="${tech.type}"/>
+      <label for="done">Выучил</label>
+    </div>
+    `
+}
+
+
+function toggleTech(event) {
+
+    const type = event.target.dataset.type
+    const tech = technologies.find(t => t.type === type)
+    tech.done = event.target.checked
+
+
+    init()
+
+
+}
+
+
+function openModal(html, title = APP_TITLE) {
+    document.title = `${title} | ${APP_TITLE}`
+    modal.innerHTML = html
     modal.classList.add('open')
 }
 
 
 function closeModal() {
+    document.title = APP_TITLE
     modal.classList.remove('open')
 }
 
@@ -89,8 +130,8 @@ function toCard(tech) {
 
 
     return `
-    <div class="card ${doneClass}" data-type"${tech.type}">
-        <h3 data-type"${tech.type}">${tech.title}</h3>
+    <div class="card ${doneClass}" data-type="${tech.type}">
+        <h3 data-type="${tech.type}">${tech.title}</h3>
     </div>   
     `
 }
